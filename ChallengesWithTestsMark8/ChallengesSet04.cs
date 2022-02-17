fe9ca7eb52ef61d06cd26969b5686632fb3c43ce
jfranco9202 <jeremy.franco41@yahoo.com>
2022-02-17 08:11:27 -0600

    fifth and 6th challenges

---
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChallengesWithTestsMark8
+{
+    public class ChallengesSet04
+    {
+        public int AddEvenSubtractOdd(int[] numbers)
+        {
+            var sum = 0;
+
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    sum += num;
+                }
+
+                if (num % 2 != 0)
+                {
+                    sum -= num;
+                }
+            }
+
+            return sum;
+        }
+
+        public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
+        {
+            var list = new List<int> { str1.Length, str2.Length, str3.Length, str4.Length };
+            return list.Min();
+
+        }
+
+        public int GetSmallestNumber(int number1, int number2, int number3, int number4)
+        {
+            var list = new List<int> {number1, number2, number3, number4};
+            return list.Min();
+        }
+
+        public void ChangeBusinessNameTo_TrueCoders(Business biz)
+        {
+            biz.Name = "TrueCoders";
+        }
+
+        public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
+        {
+            return
+            ( sideLength1 + sideLength2 > sideLength3 &&
+              sideLength1 + sideLength3 > sideLength2 &&
+              sideLength2 + sideLength3 > sideLength1
+            );
+        }
+
+        public bool IsStringANumber(string input)
+        {
+            var isNumber = double.TryParse(input, out double number);
+
+            return isNumber;
+
+        }
+
+        public bool MajorityOfElementsInArrayAreNull(object[] objs)
+        {
+            var amount = objs.Length;
+            var majority = (amount / 2) + 1;
+
+            var count = 0;
+
+            foreach(var obj in objs)
+            {
+                if(obj == null)
+                {
+                    count++;
+                }
+                
+            }
+            if(count >= majority)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public double AverageEvens(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+
+            var n = numbers.Where(num => num % 2 == 0);
+
+            if (n.Count() == 0)
+                return 0;
+            
+            return n.Average();
+
+        }
+
+        public int Factorial(int number)
+        {
+            var fact = 1;
+
+            if(number < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            for(int i = number; i > 0; i--)
+            {
+                fact *= i;
+            }
+
+            return fact;
+        }
+    }
+}
---
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChallengesWithTestsMark8
+{
+    public class ChallengesSet05
+    {
+        public int GetNextNumberDivisibleByN(int startNumber, int n)
+        {
+            if(startNumber == 0)
+            {
+                return n;
+            }
+
+            List<int> firstlist = new List<int>();
+            int maxLoop = startNumber * n;
+            int[] numbersDivisiblebyN = new int[0];
+
+            for (int i = 0; i < maxLoop; i++)
+            {
+                if(i > startNumber && i % n == 0 )
+                {
+                    firstlist.Add(i);
+
+                }
+
+                
+
+            }
+
+            firstlist.ToArray();
+            return firstlist[0];
+
+        }
+
+        public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
+        {
+            for (int i = 0; i < businesses.Length; i++)
+            {
+                if (i > businesses[i].TotalRevenue == 0)
+                {
+                    businesses[i].Add = "Closed";
+                }
+            }
+        }
+
+        public bool IsAscendingOrder(int[] numbers)
+        {
+            if(numbers == null || numbers.Length == 0)
+            {
+                return false;
+            }
+
+            for(int i = 1; i < numbers.Length; i++)
+            {
+                if(numbers[i] < numbers [i - 1] )
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public int SumElementsThatFollowAnEven(int[] numbers)
+        {
+            if(numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+
+            int sum = 0;
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if(numbers[i - 1] % 2 == 0 )
+                {
+                    sum += numbers[i];
+                }
+            }
+
+            return sum;
+        }
+
+        public string TurnWordsIntoSentence(string[] words)
+        {
+            if (words == null || words.Length == 0 )
+            {
+                return "";
+            }
+
+            int badWords = 0;
+            int goodWords = 0;
+            List<string> wordList = new List<string>();
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                if(words[i] == "" || words[i] = " " || words[i] = "  " )
+                {
+                    badWords += 1;
+                }
+
+                else
+                {
+                    wordList.Add(words[i].Trim());
+                    goodWords += 1;
+
+                }
+            }
+
+            if(goodWords == 0)
+            {
+                return "";
+            }
+            else
+            {
+                return string.Join("", wordList) + "-"; 
+            }
+
+
+        }
+
+        public double[] GetEveryFourthElement(List<double> elements)
+        {
+            if(elements == null || elements.Count == 0)
+            {
+                return new double[0];
+            }
+
+            List<double> everyFourth = new List<double>();
+            
+            for(int i = 3; i < elements.Count; i+= 4)
+            {
+                everyFourth.Add(elements[i]);
+            }
+
+            return everyFourth.ToArray();
+        }
+
+        public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
+        {
+            for(int i = 0; i < nums.Length; i++)
+                
+                for(int k = i + 1; k < nums.Length; k++ )
+                {
+                    if (nums[i] + nums[k] == targetNumber)
+                    {
+                        return true;
+                    }
+
+
+
+                }
+            return false;
+
+        }
+    }
+}
